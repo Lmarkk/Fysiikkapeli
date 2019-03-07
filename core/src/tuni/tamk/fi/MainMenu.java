7459707fa7cbd10deeb87ab846378d668df6e307
Sorellum <lassi.markkinen@hotmail.com>
2019-03-07 13:31:02 +0200

    Created main menu button rectangles Instantiated main menu button rectangles

--- core/src/tuni/tamk/fi/MainMenu.java
+++ core/src/tuni/tamk/fi/MainMenu.java
@@ -5,18 +5,28 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MainMenu implements Screen {
 
     private MyGame game;
     private SpriteBatch batch;
+    Texture button;
+    Rectangle playButtonRect;
+    Rectangle tutorialButtonRect;
+    Rectangle settingsButtonRect;
     Texture img;
 
     public MainMenu(MyGame game) {
         this.game = game;
         batch = game.getBatch();
-        img = new Texture("badlogic.jpg");
 
+        img = new Texture("badlogic.jpg");
+        button = new Texture("button.png");
+
+        playButtonRect = new Rectangle();
+        tutorialButtonRect = new Rectangle();
+        settingsButtonRect = new Rectangle();
     }
 
     @Override
@@ -26,7 +36,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
