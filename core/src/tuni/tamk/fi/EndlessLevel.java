a03657890f3a3b2cb4db75924935cab97483142c
Arttu <arttu.knuutinen@cs.tamk.fi>
2019-03-07 14:22:35 +0200

    Pasted "Body" render code

--- core/src/tuni/tamk/fi/EndlessLevel.java
+++ core/src/tuni/tamk/fi/EndlessLevel.java
@@ -1,11 +1,16 @@
 package tuni.tamk.fi;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Body;
 
 public class EndlessLevel implements Screen {
     private MyGame game;
     private SpriteBatch batch;
+
+
 
     public EndlessLevel(MyGame g) {
         game = g;
@@ -18,7 +23,28 @@ public class EndlessLevel implements Screen {
 
     @Override
     public void render(float delta) {
-
+//        for (Body body : bodies) {
+//            if(body.getUserData() != null){
+//                float radius = body.getFixtureList().get(0).getShape().getRadius();
+//                Texture texture = (Texture) body.getUserData();
+//                batch.draw(texture,
+//                        body.getPosition().x - radius,
+//                        body.getPosition().y - radius,
+//                        radius,
+//                        radius,
+//                        radius * 2f,
+//                        radius * 2f,
+//                        1.0f,
+//                        1.0f,
+//                        body.getTransform().getRotation() * MathUtils.radiansToDegrees,
+//                        0,
+//                        0,
+//                        texture.getWidth(),
+//                        texture.getHeight(),
+//                        false,
+//                        false);
+//            }
+//        }
     }
 
     @Override
@@ -44,5 +70,11 @@ public class EndlessLevel implements Screen {
     @Override
     public void dispose() {
 
+    }
+    private static void createFloor(){
+
+    }
+    private Body createBody(){
+        return null;
     }
 }
