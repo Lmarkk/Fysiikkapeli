4b9637c92044017e9c582289b34a2701c20b6b3a
Arttu <arttu.knuutinen@cs.tamk.fi>
2019-03-07 12:36:49 +0200

    Added MainMenu

--- core/src/tuni/tamk/fi/MyGame.java
+++ core/src/tuni/tamk/fi/MyGame.java
@@ -1,33 +1,38 @@
 package tuni.tamk.fi;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public class MyGame extends ApplicationAdapter {
+// TODO: tee peli
+public class MyGame extends Game {
 	SpriteBatch batch;
-	Texture img;
-	
+	MainMenu mainMenu;
+    OrthographicCamera camera;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	    camera = new OrthographicCamera(16f, 9f);
+		mainMenu = new MainMenu(this);
+        setScreen(mainMenu);
 	}
 
-	@Override
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    @Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+		super.render();
+
+    }
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
