1811b45269bee49e0ade8bd2d47ca70530bb86a8
Sorellum <lassi.markkinen@hotmail.com>
2019-03-08 12:24:21 +0200

    Removed pixelCamera

--- core/src/tuni/tamk/fi/MainMenu.java
+++ core/src/tuni/tamk/fi/MainMenu.java
@@ -18,7 +18,6 @@ public class MainMenu implements Screen {
     Rectangle tutorialButtonRect;
     Rectangle settingsButtonRect;
     OrthographicCamera meterCamera;
-    OrthographicCamera pixelCamera;
     BitmapFont mainFont;
     Texture img;
 
@@ -33,8 +32,6 @@ public class MainMenu implements Screen {
         float buttonWidth = button.getWidth()/30f;
 
         meterCamera = game.getCamera();
-        pixelCamera = new OrthographicCamera();
-        pixelCamera.setToOrtho(false, 1600, 900);
 
         mainFont = new BitmapFont();
 
@@ -53,14 +50,18 @@ public class MainMenu implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
+
         batch.setProjectionMatrix(meterCamera.combined);
         batch.draw(button, playButtonRect.x, playButtonRect.y, playButtonRect.getWidth(), playButtonRect.getHeight());
         batch.draw(button, tutorialButtonRect.x, tutorialButtonRect.y, tutorialButtonRect.getWidth(), tutorialButtonRect.getHeight());
         batch.draw(button, settingsButtonRect.x, settingsButtonRect.y, settingsButtonRect.getWidth(), settingsButtonRect.getHeight());
-        batch.setProjectionMatrix(pixelCamera.combined);
+
         batch.end();
         game.getTextRenderer().renderText("PLAY", 800f, 450f, game.getTextRenderer().getTitleFont());
+
+
     }
 
     @Override
