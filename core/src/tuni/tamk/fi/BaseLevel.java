87be29c55137cfdfc79f2ad9a2a0bde6c1ea2b62
Arttu <arttu.knuutinen@cs.tamk.fi>
2019-03-08 14:55:33 +0200

    Added inputprocessor and throwmethod for projectiles

--- core/src/tuni/tamk/fi/BaseLevel.java
+++ core/src/tuni/tamk/fi/BaseLevel.java
@@ -1,5 +1,6 @@
 package tuni.tamk.fi;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -16,7 +17,11 @@ public class BaseLevel implements Screen {
     private float accumulator;
     private float timeStep;
     private World gameWorld;
+    private Vector2 touchStart = new Vector2();
+    private Vector2 touchEnd = new Vector2();
+    private Vector2 throwDirection = new Vector2();
 
+    private Body currentProjectile = null;
 
     public BaseLevel(MyGame g) {
         game = g;
@@ -41,8 +46,30 @@ public class BaseLevel implements Screen {
 
             }
         });
+        Gdx.input.setInputProcessor(new MyInputProcessor() {
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                touchStart = new Vector2(screenX, screenY);
+                return super.touchDown(screenX, screenY, pointer, button);
+            }
+
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                touchEnd = new Vector2(screenX, screenY);
+                throwProjectile(currentProjectile);
+                return super.touchUp(screenX, screenY, pointer, button);
+            }
+        });
         accumulator = 0;
         timeStep = 1/60f;
+    }
+    public void throwProjectile(Body body){
+        if(body != null){
+            throwDirection = new Vector2(touchStart.sub(touchEnd));
+            throwDirection.y *= -1;
+            System.out.println(throwDirection);
+            body.applyLinearImpulse(throwDirection, body.getWorldCenter(), true);
+        }
     }
     public void doPhysicsStep(float deltaTime) {
         float frameTime = deltaTime;
