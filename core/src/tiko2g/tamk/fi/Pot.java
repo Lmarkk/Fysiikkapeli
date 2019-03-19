221bbbace4895d78dab199477e8fc0ab16c87873
Arttu <arttu.knuutinen@cs.tamk.fi>
2019-03-19 07:54:03 +0200

    WIP pot texture implementation. Replaced most magic numbers from pot creation.

--- core/src/tiko2g/tamk/fi/Pot.java
+++ core/src/tiko2g/tamk/fi/Pot.java
@@ -12,7 +12,9 @@ public class Pot {
     private MyGame game;
     private SpriteBatch batch;
     private BaseLevel baseLevel;
-    private Texture potTexture;
+    private Texture potBottomTexture;
+    private Texture potTopTexture;
+    private Texture debugTexture;
     private Body leftSide;
     private Body rightSide;
     private Body bottom;
@@ -24,8 +26,10 @@ public class Pot {
         game = g;
         batch = g.getBatch();
         baseLevel = b;
-        potTexture = new Texture("groundtexture.png");
-        createPot(10, 1);
+        potBottomTexture = new Texture("kattila_bottom.png");
+        potTopTexture = new Texture("kattila_top.png");
+        debugTexture = new Texture("badlogic.jpg");
+        createPot(10, 1, 0.33f);
 
     }
     public BodyDef getPotBodyDef(float x, float y) {
@@ -45,24 +49,29 @@ public class Pot {
         potFixtureDef.shape = polygonShape;
         return potFixtureDef;
     }
-    public void createPot(float x, float y) {
+    public void createPot(float x, float y, float scale) {
+        float sideRectWidth = 0.2f;
+        float sideRectHeight = 2.5f;
+        float bottomRectHeight = 0.5f;
+        float potWidth = potBottomTexture.getWidth() / 100f * scale;
+        leftSide = baseLevel.getGameWorld().createBody(getPotBodyDef(x - sideRectWidth / 2f, y + sideRectHeight / 2f));
+        rightSide = baseLevel.getGameWorld().createBody(getPotBodyDef(x + (potWidth + sideRectWidth / 2f), y + sideRectHeight / 2f));
+        bottom = baseLevel.getGameWorld().createBody(getPotBodyDef(x + potWidth / 2f, y + bottomRectHeight / 2f));
 
-        leftSide = baseLevel.getGameWorld().createBody(getPotBodyDef(x+0.25f, y+1.75f));
-        rightSide = baseLevel.getGameWorld().createBody(getPotBodyDef(x+2.75f, y+1.75f));
-        bottom = baseLevel.getGameWorld().createBody(getPotBodyDef(x+1.5f, y+0.25f));
+        leftSide.createFixture(getPotFixtureDef(sideRectWidth / 2f, sideRectHeight / 2f));
+        rightSide.createFixture(getPotFixtureDef(sideRectWidth / 2f,sideRectHeight / 2f));
+        bottom.createFixture(getPotFixtureDef(potWidth / 2f,bottomRectHeight / 2f));
 
-        leftSide.createFixture(getPotFixtureDef(0.25f,1.25f));
-        rightSide.createFixture(getPotFixtureDef(0.25f,1.25f));
-        bottom.createFixture(getPotFixtureDef(1.5f,0.25f));
-
-        leftSideRect = new Rectangle(x, y+0.5f, 0.5f, 2.5f);
-        rightSideRect = new Rectangle(x+2.5f, y+0.5f, 0.5f, 2.5f);
-        bottomRect = new Rectangle(x, y, 3, 0.5f);
+        leftSideRect = new Rectangle(x - sideRectWidth, y, sideRectWidth, sideRectHeight);
+        rightSideRect = new Rectangle(x + potWidth, y, sideRectWidth, sideRectHeight);
+        bottomRect = new Rectangle(x, y, potWidth, bottomRectHeight);
     }
 
     public void draw() {
-        batch.draw(potTexture, leftSideRect.x, leftSideRect.y, leftSideRect.getWidth(), leftSideRect.getHeight());
-        batch.draw(potTexture, rightSideRect.x, rightSideRect.y, rightSideRect.getWidth(), rightSideRect.getHeight());
-        batch.draw(potTexture, bottomRect.x, bottomRect.y, bottomRect.getWidth(), bottomRect.getHeight());
+        batch.draw(potBottomTexture, bottomRect.x, bottomRect.y - 0.75f, bottomRect.getWidth(), bottomRect.getHeight() * 6);
+        batch.draw(potTopTexture, bottomRect.x, bottomRect.y - 0.75f, bottomRect.getWidth(), bottomRect.getHeight() * 6);
+        batch.draw(debugTexture, leftSideRect.x, leftSideRect.y, leftSideRect.getWidth(), leftSideRect.getHeight());
+        batch.draw(debugTexture, rightSideRect.x, rightSideRect.y, rightSideRect.getWidth(), rightSideRect.getHeight());
+        batch.draw(debugTexture, bottomRect.x, bottomRect.y, bottomRect.getWidth(), bottomRect.getHeight());
     }
 }
