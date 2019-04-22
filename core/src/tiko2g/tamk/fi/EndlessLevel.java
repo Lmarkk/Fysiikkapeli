429800e6719301a844f770f133ac4817a7cb1856
Sorellum <lassi.markkinen@hotmail.com>
2019-04-22 17:23:03 +0300

    Adjusted ground length, border wall position and landing timer

--- core/src/tiko2g/tamk/fi/EndlessLevel.java
+++ core/src/tiko2g/tamk/fi/EndlessLevel.java
@@ -54,7 +54,7 @@ public class EndlessLevel extends BaseLevel {
         setNextProjectile();
 
         createBorderWall(-0.5f, 0);
-        createBorderWall(48f, 0);
+        createBorderWall(120f, 0);
 
 
     }
--- core/src/tiko2g/tamk/fi/FirstLevel.java
+++ core/src/tiko2g/tamk/fi/FirstLevel.java
@@ -50,7 +50,7 @@ public class FirstLevel extends BaseLevel {
         setNextProjectile();
 
         createBorderWall(-0.5f, 0);
-        createBorderWall(48f, 0);
+        createBorderWall(120f, 0);
     }
     @Override
     public void render(float delta) {
